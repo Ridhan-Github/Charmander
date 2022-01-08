bb45f5fcae3dba184d4e4379047e452a09b2dec8
ᗪᗩᖇK ✞Oᖇᗪ <85796566+Judson-web@users.noreply.github.com>
2022-01-08 10:31:03 +0530

    Update text.py

--- renamer/tools/text.py
+++ renamer/tools/text.py
@@ -4,23 +4,24 @@ class TEXT:
     DOWNLOAD_START = f"**📥 ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ...**"
     UPLOAD_START = f"**📤 ᴜᴘʟᴏᴀᴅɪɴɢ...**"
     UPLOAD_SUCESS = f"ᴛʜᴀɴᴋ ʏᴏᴜ ꜰᴏʀ ᴜꜱɪɴɢ ᴍᴇ | ꜱʜᴀʀᴇ > ⚡"
-    BANNED_USER_TEXT = f"Hey, you are **banned** from using me {FACE_WITH_TEARS_OF_JOY}."
-    NOT_LOGGED_TEXT = f"This bot was only for private use {LOCKED_WITH_KEY}. If you want to use this bot you need to send me correct password in the format `/login bot_password`"
+    BANNED_USER_TEXT = f"Hᴇʏ, ʏᴏᴜ ᴀʀᴇ **ʙᴀɴɴᴇᴅ** ғʀᴏᴍ ᴜsɪɴɢ ᴍᴇ {FACE_WITH_TEARS_OF_JOY}."
+    NOT_LOGGED_TEXT = f"Tʜɪs ʙᴏᴛ ᴡᴀs ᴏɴʟʏ ғᴏʀ ᴘʀɪᴠᴀᴛᴇ ᴜsᴇ {LOCKED_WITH_KEY}, Iғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴜsᴇ ᴛʜɪs ʙᴏᴛ ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ sᴇɴᴅ ᴍᴇ ᴄᴏʀʀᴇᴄᴛ ᴘᴀssᴡᴏʀᴅ ɪɴ ᴛʜᴇ ғᴏʀᴍᴀᴛ `/login ᴘᴀssᴡᴏʀᴅ`"
     SAVED_CUSTOM_THUMBNAIL = f"**✅ ᴛʜᴜᴍʙɴᴀɪʟ ꜱᴀᴠᴇᴅ ᴘᴇʀᴍᴀɴᴇɴᴛʟʏ**"
     DELETED_CUSTOM_THUMBNAIL = f"**🗑️ ᴛʜᴜᴍʙɴᴀɪʟ ᴅᴇʟᴇᴛᴇᴅ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ**"
     NO_CUSTOM_THUMB_NAIL_FOUND = f"**🙅‍♂️ ɴᴏ ᴛʜᴜᴍʙɴᴀɪʟ ꜰᴏᴜɴᴅ"
     THUMBNAIL_CAPTION = f"**📲 ʏᴏᴜʀ ᴘᴇʀᴍᴀɴᴇɴᴛ ᴛʜᴜᴍʙɴᴀɪʟ**"
 
 
-    ABOUT = """**Bot Info**
-
-** ➜ My Name:** {bot_name}
-** ➜ Language:** [Python 3](https://www.python.org/)
-** ➜ FrameWork:** [Pyrogram](https://github.com/pyrogram/pyrogram)
-** ➜ Developer:** {bot_owner}
-** ➜ Channel:** [🎥Story Time channel Files 🎥](https://t.me/storytimeoG)
-** ➜ Source Code:** [Press Me](https://t.me/NOKIERUNNOIPPKITTUM)
-"""
+    ABOUT = """
+╔════❰ Aʙᴏᴜᴛ ❱═❍⊱❁۪۪
+║╭━━━━━━━━━━━━━━━➣ 
+║┣⪼ Mʏ Nᴀᴍᴇ: - <a href="https://t.me/RenamerPro_V2_bot">Rᴇɴᴀᴍᴇʀ Pʀᴏ</a>
+║┣⪼ Cʀᴇᴀᴛᴏʀ - <a href=https://t.me/VAMPIRE_KING_NO_1>Tᴇʀʀᴏʀ Mɪᴄᴋᴇʏ</a>
+║┣⪼ Lɪʙʀᴀʀʏ - <a href="https://docs.pyrogram.org/"> Pʏʀᴏɢʀᴀᴍ </a>
+║┣⪼ Lᴀɴɢᴜᴀɢᴇ - Pʏᴛʜᴏɴ 3 
+║┣⪼ ⲂⲞⲦ Sᴇʀᴠᴇʀ - Hᴇʀᴏᴋᴜ
+║╰━━━━━━━━━━━━━━━➣ 
+╚══════════════════❍⊱❁۪۪"""
 
     HELP_USER = """**🚨 ᴏʜ! ʏᴏᴜ ɴᴇᴇᴅ ʜᴇʟᴘ!**
    
@@ -29,25 +30,25 @@ class TEXT:
 ➜ ɴᴏᴡ ꜱᴇɴᴅ ᴍᴇ ᴛʜᴇ ᴛᴇʟᴇɢʀᴀᴍ ꜰɪʟᴇ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ʀᴇɴᴀᴍᴇ.
 ➜ ꜱᴇɴᴅ ᴛʜᴇ ɴᴇᴡ ɴᴀᴍᴇ ᴡʜᴇɴ ʙᴏᴛ ᴀꜱᴋ.
 
-ꜰᴏʀ ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ᴄʜᴇᴄᴋ ᴀʙᴏᴜᴛ ꜱᴇᴄᴛɪᴏɴ!
+ᴘᴏᴡᴇʀᴅ ʙʏ <a href=https://t.me/VAMPIRE_KING_NO_1>Tᴇʀʀᴏʀ Mɪᴄᴋᴇʏ</a>
 """
 
-    START_TEXT = """Hey! {user_mention}
-I'AM CHARMANDER 
-TELEGRAM RENAMER BOT WITH PERMANENT THUMBNAIL SUPPORT!🚀.
+    START_TEXT = """Hʟᴏ! {user_mention}
+I'ᴀᴍ Cʜᴀʀᴍᴀɴᴅᴇʀ
+TA Tᴇʟᴇɢʀᴀᴍ Fᴀsᴛ Rᴇɴᴀᴍᴇ Bᴏᴛ Wɪᴛʜ Pᴇʀᴍᴀɴᴇɴᴛ Tʜᴜᴍʙɴᴀɪʟ Sᴜᴘᴘᴏʀᴛ!🚀.
 
-<b>DO /help FOR MOR DETAILS...</b>
-**owner:** {bot_owner}
+<b>Sᴇɴᴅ /help Fᴏʀ Mᴏʀᴇ Dᴇᴛᴀɪʟs...</b>
+**Oᴡɴᴇʀ:** {bot_owner}
 """
 
 
-    DONATE_USER = """**Hey Bruh/Sis **
+    DONATE_USER = """**Hᴇʏ Bʀᴜʜ/Sɪs**
 
-🌀 DONATE ME IF YOU LIKE THIS BOT!
+🌀 Dᴏɴᴀᴛᴇ Mᴇ Iғ Yᴏᴜ Lɪᴋᴇ Tʜɪs Bᴏᴛ!
 
 💸 10ʀꜱ
  
 **ᴘᴀʏᴍᴇɴᴛ ᴍᴇᴛʜᴏᴅ**
 ɢᴏᴏɢʟᴇᴘᴀʏ / ᴘᴀʏᴛᴍ / ᴘʜᴏɴᴘᴀʏ 
  
-**CONTACT OWNER:** [౮คɱƿɿՐ૯ қɿՈ૭ 👑 #𝚂𝙴𝙳](https://t.me/VAMPIRE_KING_NO_1)"""
+**CONTACT OWNER:** [Tᴇʀʀᴏʀ Mɪᴄᴋᴇʏ](https://t.me/VAMPIRE_KING_NO_1)"""
